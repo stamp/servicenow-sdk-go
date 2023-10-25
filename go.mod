e99e03b0b6d92e0b405115e0432ac2e9817f28fb
Michael Canady <michaeldcanady+github@gmail.com>
2023-10-25 18:43:53 -0400

    adding now request builder example

--- go.mod
+++ go.mod
@@ -12,6 +12,10 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- now_request_builder.go
+++ now_request_builder.go
@@ -10,7 +10,20 @@ type NowRequestBuilder struct {
 }
 
 // NewNowRequestBuilder creates a new instance of the NowRequestBuilder associated with the given URL and Client.
-// It accepts the URL and Client as parameters and returns a pointer to the created NowRequestBuiabstraction
+// It accepts the URL and Client as parameters and returns a pointer to the created NowRequestBuilder.
+//	package servicenowsdkgo
+
+// import (
+//
+//	servicenowsdkgo "github.com/michaeldcanady/servicenow-sdk-go"
+//
+// )
+//
+//	func main() {
+//		url := "instance.service-now.com/api"
+//		client := servicenowsdkgo.NewClient()
+//		nowRequestBuilder := servicenowsdkgo.NewNowrequestBuilder(url, client)
+//	}
 func NewNowRequestBuilder(url string, client *ServiceNowClient) *NowRequestBuilder {
 	pathParameters := map[string]string{"baseurl": url}
 	requestBuilder := core.NewRequestBuilder(client, "{+baseurl}/Now", pathParameters)
